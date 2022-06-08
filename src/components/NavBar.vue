d678854fc34a7bdbe721851f19eb24498ead056c
FouLin <foulin.tm@gmail.com>
2022-06-08 20:01:44 +0700

    С ноута

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,30 +1,59 @@
 <template>
-  <b-navbar toggleable="sm" type="light" variant="light">
-    <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
+  <div id="nav-bar">
+    <b-navbar toggleable="sm" type="light" variant="light">
+      <b-navbar-toggle target="nav-text-collapse"></b-navbar-toggle>
 
-    <b-navbar-brand>главная страница</b-navbar-brand>
+      <b-navbar-brand>главная страница</b-navbar-brand>
 
-    <b-collapse id="nav-text-collapse" is-nav>
-      <b-navbar-nav>
-        <b-nav-text>{{ $store.state.token }}</b-nav-text>
-      </b-navbar-nav>
-    </b-collapse>
+      <b-collapse id="nav-text-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-text>{{ $store.state.token }}</b-nav-text>
+        </b-navbar-nav>
+      </b-collapse>
 
-    <b-nav-item-dropdown text="Lang" right>
-      <b-dropdown-item>EN</b-dropdown-item>
-      <b-dropdown-item>EN</b-dropdown-item>
-      <b-dropdown-item>EN</b-dropdown-item>
-      <b-dropdown-item>EN</b-dropdown-item>
-      <b-dropdown-item>EN</b-dropdown-item>
-    </b-nav-item-dropdown>
+      <b-nav-item-dropdown text="Lang" right>
+        <b-dropdown-item>EN</b-dropdown-item>
+        <b-dropdown-item>EN</b-dropdown-item>
+        <b-dropdown-item>EN</b-dropdown-item>
+        <b-dropdown-item>EN</b-dropdown-item>
+        <b-dropdown-item>EN</b-dropdown-item>
+      </b-nav-item-dropdown>
 
-    <b-button variant="outline-primary" v-b-modal.modal-1 right>EDIT</b-button>
+      <b-button variant="outline-primary" v-b-modal.modal-1 right>EDIT</b-button>
 
-    <b-modal id="modal-1" title="Редактор локаций">
-
-    </b-modal>
-
-  </b-navbar>
+      <b-modal id="modal-1" class="content-in-dialog" title="Редактор локаций">
+        <b-list-group flush>
+          <b-list-group-item href="#">Cras justo odio</b-list-group-item>
+          <b-list-group-item href="#">Dapibus ac facilisis in</b-list-group-item>
+          <b-list-group-item href="#">Vestibulum at eros</b-list-group-item>
+          <b-list-group-item href="#">Cras justo odio</b-list-group-item>
+          <b-list-group-item href="#">Dapibus ac facilisis in</b-list-group-item>
+          <b-list-group-item href="#">Vestibulum at eros</b-list-group-item>
+          <b-list-group-item href="#">Cras justo odio</b-list-group-item>
+          <b-list-group-item href="#">Dapibus ac facilisis in</b-list-group-item>
+          <b-list-group-item href="#">Vestibulum at eros</b-list-group-item>
+          <b-list-group-item href="#">Cras justo odio</b-list-group-item>
+          <b-list-group-item href="#">Dapibus ac facilisis in</b-list-group-item>
+          <b-list-group-item href="#">Vestibulum at eros</b-list-group-item>
+          <b-list-group-item href="#">Cras justo odio</b-list-group-item>
+          <b-list-group-item href="#">Dapibus ac facilisis in</b-list-group-item>
+          <b-list-group-item href="#">Vestibulum at eros</b-list-group-item>
+          <b-list-group-item href="#">Cras justo odio</b-list-group-item>
+          <b-list-group-item href="#">Dapibus ac facilisis in</b-list-group-item>
+          <b-list-group-item href="#">Vestibulum at eros</b-list-group-item>
+          <b-list-group-item href="#">Cras justo odio</b-list-group-item>
+          <b-list-group-item href="#">Dapibus ac facilisis in</b-list-group-item>
+          <b-list-group-item href="#">Vestibulum at eros</b-list-group-item>
+          <b-list-group-item href="#">Cras justo odio</b-list-group-item>
+          <b-list-group-item href="#">Dapibus ac facilisis in</b-list-group-item>
+          <b-list-group-item href="#">Vestibulum at eros</b-list-group-item>
+          <b-list-group-item href="#">Cras justo odio</b-list-group-item>
+          <b-list-group-item href="#">Dapibus ac facilisis in</b-list-group-item>
+          <b-list-group-item href="#">Vestibulum at eros</b-list-group-item>
+        </b-list-group>
+      </b-modal>
+    </b-navbar>
+  </div>
 </template>
 
 <script lang="ts">
@@ -36,6 +65,9 @@ export default class NavBar extends Vue{
 }
 </script>
 
-<style scoped>
-
+<style lang="sass">
+#nav-bar
+  #modal-1___BV_modal_outer_
+    height: 50vh
+    overflow: auto
 </style>
--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import vuetify from './plugins/vuetify' // path to vuetify export
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
@@ -12,6 +13,7 @@ Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
 new Vue({
+  vuetify,
   router,
   store,
   render: h => h(App)
